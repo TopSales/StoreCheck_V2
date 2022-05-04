fafb6b49706bc5449cdc917145aba38967046847
Michael <Michael@ZPF.fr>
2022-05-04 15:16:38 +0200

    divers chat

--- Server/ServerWPF/Modeles/Params.cs
+++ Server/ServerWPF/Modeles/Params.cs
@@ -8,10 +8,17 @@ public class Params
 {
    public string CurrentMessage { get; set; } = "En maintennance ...";
 
-   #region - - - server - - -
-   public string ClientName { get; internal set; } = "Client";
-   public string ServerIP { get; set; } = "127.0.0.1";
-   public string ServerPort { get; set; } = "9000";
-   #endregion
+      #region - - - server - - -
+      public string ClientName { get; internal set; } = "Client";
+
+#if DEBUG
+      public string ServerIP { get; set; } = "127.0.0.1";
+      public string ServerPort { get; set; } = "9000";
+#else
+      public string ServerIP { get; set; } = "69.10.45.253";
+      public string ServerPort { get; set; } = "9000";
+#endif
+
+      #endregion
 }
 
--- Terminal/StoreCheck/Models/Params.cs
+++ Terminal/StoreCheck/Models/Params.cs
@@ -21,7 +21,20 @@ namespace ZPF
 
       void SetUserID()
       {
-         Microsoft.AppCenter.AppCenter.SetUserId( $"{Login}={UserFK}");
+         Microsoft.AppCenter.AppCenter.SetUserId($"{Login}={UserFK}");
       }
+
+      #region - - - server - - -
+      public string ClientName { get; internal set; } = "Client";
+
+#if DEBUG
+      public string ServerIP { get; set; } = "127.0.0.1";
+      public string ServerPort { get; set; } = "9000";
+#else
+      public string ServerIP { get; set; } = "69.10.45.253";
+      public string ServerPort { get; set; } = "9000";
+#endif
+
+      #endregion
    }
 }
