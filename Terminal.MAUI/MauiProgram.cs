68ee98dbb004ddbe416395e1da1f29c0e21e632e
Michael <Michael@ZPF.fr>
2022-05-22 15:39:18 +0200

    Tuiles du pauvre ...

--- Terminal.MAUI/MauiProgram.cs
+++ Terminal.MAUI/MauiProgram.cs
@@ -2,17 +2,21 @@
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+   public static MauiApp CreateMauiApp()
+   {
+      var builder = MauiApp.CreateBuilder();
+      builder
+         .UseMauiApp<App>()
+         .ConfigureFonts(fonts =>
+         {
+            fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            fonts.AddFont("desyrel.ttf", "desyrel");
+            fonts.AddFont("IconFont.ttf", "IconFont");
+            fonts.AddFont("MediaPlayerFont.ttf", "MediaPlayerFont");
+            fonts.AddFont("Montserrat.ttf", "Montserrat");
+         });
 
-		return builder.Build();
-	}
+      return builder.Build();
+   }
 }
