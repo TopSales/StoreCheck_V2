3642cbe64b16f83d3de048ed88a353d6bdea3c4c
Michael <Michael@ZPF.fr>
2022-05-22 11:09:58 +0200

    Test scan

--- Terminal.MAUI/Pages/EANPage.xaml.cs
+++ Terminal.MAUI/Pages/EANPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using System.Xml.Linq;
 using ZPF;
 using ZPF.SQL;
 
@@ -34,37 +35,27 @@ public partial class EANPage : ContentPage
       {
          OnAppearing_sema = false;
 
-         //   //if (MainViewModel.Current.ArticlesEAN.Count == 0)
-         //   //{
-         //   //   SyncEAN();
-         //   //};
-
          if (EANViewModel.Current.ArticlesEAN == null || EANViewModel.Current.ArticlesEAN.Count == 0)
          {
             EANViewModel.Current.CurrentArticleEAN = null;
 
             EANViewModel.Current.SetArticlesEAN();
          };
-
-         //   OnAppearing_sema = true;
       };
-
-      ////DependencyService.Get<IScanner>().OpenScanner();
-      ////DependencyService.Get<IScanner>().EnableAllSymbologies();
 
       UnitechViewModel.Current.OnScann += OnScann;
 
-      //DoIt.Delay(100, () =>
-      //{
-      //   DoIt.OnMainThread(() =>
-      //   {
-      //#if SCAN_WEDGE
       entry.Unfocused -= Entry_Unfocused;
       entry.Unfocused += Entry_Unfocused;
-      //#endif
-      entry.Focus();
-      //   });
-      //});
+
+      ZPF.DoIt.Delay(1000, () =>
+      {
+         ZPF.DoIt.OnMainThread(() =>
+         {
+            entry.BackgroundColor = Microsoft.Maui.Graphics.Colors.White;
+            entry.Focus();
+         });
+      });
    }
 
    protected override void OnDisappearing()
@@ -78,7 +69,6 @@ public partial class EANPage : ContentPage
 
    private void Entry_Completed(object sender, EventArgs e)
    {
-      var entry = sender as Entry;
       entry.Text = entry.Text.Trim();
 
       OnScann(entry.Text, entry.Text.Length, UnitechViewModel.Symbologies.Unknown, Encoding.ASCII.GetBytes(entry.Text));
@@ -86,15 +76,12 @@ public partial class EANPage : ContentPage
 
    private void Entry_Focused(object sender, FocusEventArgs e)
    {
-      var entry = sender as Entry;
-
       entry.CursorPosition = 0;
       entry.SelectionLength = (entry.Text == null ? 0 : entry.Text.Length);
    }
 
    private void entry_TextChanged(object sender, TextChangedEventArgs e)
    {
-      var entry = sender as Entry;
       entry.Unfocused -= Entry_Unfocused;
 
       UnitechViewModel.Current.Length = entry.Text.Length;
@@ -108,6 +95,9 @@ public partial class EANPage : ContentPage
          {
             if (!btnBack.IsFocused)
             {
+               entry.CursorPosition = 0;
+               entry.SelectionLength = (entry.Text == null ? 0 : entry.Text.Length);
+
                entry.Focus();
             };
          });
@@ -127,16 +117,18 @@ public partial class EANPage : ContentPage
       {
          slArticleEAN.BindingContext = null;
 
-         if (DeviceInfo.Platform == DevicePlatform.Android)
-         {
-            EANViewModel.Current.CurrentArticleEAN = DB_SQL.QueryFirst<EAN_Article>( EANViewModel.Current.DBSQLViewModel, $"select * from EAN_Article where EAN = '{data}'");
-         }
-         else
-         {
-            EANViewModel.Current.CurrentArticleEAN = EANViewModel.Current.ArticlesEAN.Where(x => x.EAN == data).FirstOrDefault();
-         };
+         EANViewModel.Current.CurrentArticleEAN = EANViewModel.Current.GetRecord(EANViewModel.Current.TxtFileName, data);
 
-         if (EANViewModel.Current.CurrentArticleEAN == null)
+         //if (DeviceInfo.Platform == DevicePlatform.Android)
+         //{
+         //   EANViewModel.Current.CurrentArticleEAN = DB_SQL.QueryFirst<EAN_Article>( EANViewModel.Current.DBSQLViewModel, $"select * from EAN_Article where EAN = '{data}'");
+         //}
+         //else
+         //{
+         //   EANViewModel.Current.CurrentArticleEAN = EANViewModel.Current.ArticlesEAN.Where(x => x.EAN == data).FirstOrDefault();
+         //};
+
+         if (EANViewModel.Current.CurrentArticleEAN == null || string.IsNullOrEmpty(EANViewModel.Current.CurrentArticleEAN.Label_FR))
          {
             EANViewModel.Current.CurrentArticleEAN = new EAN_Article
             {
@@ -147,6 +139,8 @@ public partial class EANPage : ContentPage
 
          slArticleEAN.BindingContext = EANViewModel.Current.CurrentArticleEAN;
       };
+
+      Entry_Focused(entry, null);
 
       return true;
    }
