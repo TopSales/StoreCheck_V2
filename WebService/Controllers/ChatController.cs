5f13a772b5c64304e995c38fddd8af727bfb53dd
Michael <Michael@ZPF.fr>
2022-07-24 22:05:40 +0200

    ....

--- WebService/Controllers/ChatController.cs
+++ WebService/Controllers/ChatController.cs
@@ -3,6 +3,8 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Reflection.Metadata;
+using System.Text;
+using System.Xml.Linq;
 using ZPF;
 using ZPF.SQL;
 
@@ -17,7 +19,30 @@ namespace StoreCheck
         {
             if (!string.IsNullOrEmpty(authorization) && MainViewModel.Current.CheckAuthorization(authorization))
             {
-                //return wsViewModel.Current.PostContact(json);
+                if (Request.Form.Files.Count == 1)
+                {
+                    var formFile = Request.Form.Files.First();
+
+                    if (formFile.Length > 0)
+                    {
+                        string json = "";
+
+                        using (var inputStream = formFile.OpenReadStream())
+                        {
+                            // stream to byte array
+                            byte[] array = new byte[inputStream.Length];
+                            inputStream.Seek(0, SeekOrigin.Begin);
+                            inputStream.Read(array, 0, array.Length);
+
+                            json = Encoding.UTF8.GetString(array);
+                        };
+
+                        if( !string.IsNullOrEmpty(json))
+                        {
+
+                        };
+                    };
+                };
 
                 return "holla";
             }
